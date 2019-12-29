5a64a371562d3bef52a7cbd3b2c14e285f4e170f
Burak Seyhan <seyhanburak9@gmail.com>
2019-12-30 00:50:30 +1100

    complete website

---
+++ app.js
@@ -0,0 +1,70 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const request = require("request");
+
+const app = express();
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.static("public"));
+
+app.get("/", function(req, res) {
+  res.sendFile(__dirname + "/signup.html");
+});
+
+app.post("/", function(req, res) {
+  var firstName = req.body.firstName;
+  var lastName = req.body.lastName;
+  var email = req.body.email;
+
+  var data = {
+    members: [
+      {
+        email_address: email,
+        status: "subscribed",
+        merge_fields: {
+          FNAME: firstName,
+          LNAME: lastName
+        }
+      }
+    ]
+  };
+
+  var jsonData = JSON.stringify(data);
+
+  var options = {
+    url: "https://us4.api.mailchimp.com/3.0/lists/ae8bec135c",
+    method: "POST",
+    // simplest way to ensure authorization in nodejs
+    headers: {
+      Authorization: "Basic e494f9fb110436b8973ec13696326c47-us"
+    },
+    body: jsonData
+  };
+
+  request(options, function(error, response, body) {
+    if (error) {
+      res.sendFile(__dirname + "/failure.html");
+    } else {
+      if (response.statusCode == 200) {
+        res.sendFile(__dirname + "/success.html");
+      } else {
+        res.sendFile(__dirname + "/failure.html");
+      }
+    }
+  });
+});
+
+// when failure redirect user to the home route
+app.post("/failure", function(req, res) {
+  res.redirect("/");
+});
+
+app.listen(3000, function() {
+  console.log("Server is started at port 3000");
+});
+
+// API key
+// e494f9fb110436b8973ec13696326c47-us4;
+
+// Audience id
+// ae8bec135c;
